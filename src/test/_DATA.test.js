f5eec493e4547f6c759951f07975d1f2adf69066
Huynh Nhat Hoa <huynhnhathoait68@gmail.com>
2023-07-13 16:45:24 +0700

    add test data.js

--- src/test/_DATA.test.js
+++ src/test/_DATA.test.js
@@ -1,4 +1,4 @@
-const { _saveQuestion } = require("../util/_DATA");
+const { _saveQuestion, _saveQuestionAnswer, _getQuestions, _getUsers } = require("../util/_DATA");
 describe("_saveQuestion", () => {
     it("should return true for correct parameters", async () => {
         const response = await _saveQuestion({
@@ -14,6 +14,31 @@ describe("_saveQuestion", () => {
                 text: 'have horrible long term memory'
             }
         });
+        expect(response).toBeTruthy();
+    });
+});
+
+describe("_saveQuestionAnswer", () => {
+    it("_saveQuestionAnswer correct parameters", async () => {
+        const response = await _saveQuestionAnswer({
+            authedUser: 'sarahedo',
+            qid: '8xf0y6ziyjabvozdd253nd',
+            answer: 'optionOne'
+        });
+        expect(response).toEqual(undefined);
+    });
+});
+
+describe("_getQuestions", () => {
+    it("_getQuestions correct parameters", async () => {
+        const response = await _getQuestions();
+        expect(response).toBeTruthy();
+    });
+});
+
+describe("_getUsers", () => {
+    it("_getUsers correct parameters", async () => {
+        const response = await _getUsers();
         expect(response).toBeTruthy();
     });
 });
