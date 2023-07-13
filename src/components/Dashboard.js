4e3b1b9d33e72642bddb6ecc8129499e658572ef
Huynh Nhat Hoa <huynhnhathoait68@gmail.com>
2023-07-13 16:04:51 +0700

    modify dashboard add readme

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,16 @@
+import {
+    Tab,
+    TabPanel,
+    Tabs,
+    TabsBody,
+    TabsHeader,
+} from "@material-tailwind/react";
+import { useState } from "react";
 import { connect } from "react-redux";
 import Card from "./Card";
 
 const Dashboard = ({ authedUser, questions, users }) => {
+    const [activeTab, setActiveTab] = useState("newquestions");
 
     const unanswered = (question) => (!question.optionOne.votes.includes(authedUser.id)
         && !question.optionTwo.votes.includes(authedUser.id));
@@ -9,31 +18,55 @@ const Dashboard = ({ authedUser, questions, users }) => {
     const answered = (question) => (question.optionOne.votes.includes(authedUser.id)
         || question.optionTwo.votes.includes(authedUser.id));
 
+    const data = [
+        {
+            label: "New Questions",
+            value: "newquestions",
+            desc: unanswered
+        },
+        {
+            label: "Answered Questions",
+            value: "answeredquestions",
+            desc: answered,
+        },
+    ]
+
 
     return (
-        <div className="text-center">
-            <h2 className="text-2xl font-bold mt-6">New Questions</h2>
-            <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                {questions
-                    .filter(unanswered)
-                    .map((question) => (
-                        <li key={question.id}>
-                            <Card question={question} author={users[question.author]} />
-                        </li>
-                    ))}
-            </ul>
-
-            <h2 className="text-2xl font-bold mt-6">Answered Questions</h2>
-            <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                {questions
-                    .filter(answered)
-                    .map((question) => (
-                        <li key={question.id}>
-                            <Card question={question} author={users[question.author]} />
-                        </li>
-                    ))}
-            </ul>
-        </div>
+        <Tabs value={activeTab}>
+            <TabsHeader
+                className="rounded-none border-b border-blue-gray-50 bg-transparent p-0"
+                indicatorProps={{
+                    className: "bg-transparent border-b-2 border-blue-500 shadow-none rounded-none",
+                }}
+            >
+                {data.map(({ label, value }) => (
+                    <Tab
+                        key={value}
+                        value={value}
+                        onClick={() => setActiveTab(value)}
+                        className={activeTab === value ? "text-blue-500" : ""}
+                    >
+                        {label}
+                    </Tab>
+                ))}
+            </TabsHeader>
+            <TabsBody>
+                {data.map(({ value, desc }) => (
+                    <TabPanel key={value} value={value}>
+                        <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                            {questions
+                                .filter(desc)
+                                .map((question) => (
+                                    <li key={question.id}>
+                                        <Card question={question} author={users[question.author]} />
+                                    </li>
+                                ))}
+                        </ul>
+                    </TabPanel>
+                ))}
+            </TabsBody>
+        </Tabs>
     );
 }
 
